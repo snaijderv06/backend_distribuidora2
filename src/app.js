ddad79b62fad9c264c4ea3c34bc60f7adc023234
snaijder <snaijdervargas46@gmail.com>
2025-05-13 15:10:50 -0600

    App .js modificacion

--- src/app.js
+++ src/app.js
@@ -28,6 +28,8 @@ app.use('/api', rutasDetallesVentas);
 app.use('/api', rutasEmpleados);
 app.use('/api', rutasDetallesCompras);
 app.use('/api', rutasCompras);
+app.use(express.json({ limit: '10mb' })); // Aumenta a 10 MB
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Manejo de rutas no encontradas
 app.use((req, res, next) => {
